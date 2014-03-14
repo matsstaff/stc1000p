c90d4a9d3f1a5606fae69d9c08b596cdeab9296b
mats <mats@matss>
2014-03-14 15:08:14 +0100

    Refactored and cleaned up. Trying to see if make stack space smaller.

--- Makefile
+++ Makefile
@@ -1,10 +1,9 @@
-#test
 CC=sdcc
-CFLAGS_C=--use-non-free -mpic14 -p16f1828 --opt-code-size --no-pcode-opt
+CFLAGS_C=--use-non-free -mpic14 -p16f1828 --opt-code-size --no-pcode-opt --stack-size 12
 CFLAGS_F=$(CFLAGS_C) -DFAHRENHEIT
-DEPS = 
-OBJ_C = page0_c.o page1_c.o
-OBJ_F = page0_f.o page1_f.o
+DEPS = stc1000p.h
+OBJ_C = page0_c.o page1_c.o eepromdata_c.o
+OBJ_F = page0_f.o page1_f.o eepromdata_f.o
 
 %_c.o: %.c $(DEPS)
 	$(CC) -c -o $@ $< $(CFLAGS_C)
@@ -16,7 +15,7 @@ stc1000p_celsius: $(OBJ_C)
 	$(CC) -o $@ $^ $(CFLAGS_C) $(LIBS)
 
 stc1000p_fahrenheit: $(OBJ_F)
-	$(CC) -o $@ $^ $(CFLAGS_F) -DFAHRENHEIT $(LIBS)
+	$(CC) -o $@ $^ $(CFLAGS_F) $(LIBS)
 
 all: stc1000p_celsius stc1000p_fahrenheit
 
---
+++ eepromdata.c
@@ -0,0 +1,58 @@
+/*
+ * STC1000+, improved firmware and Arduino based firmware uploader for the STC-1000 dual stage thermostat.
+ *
+ * Copyright 2014 Mats Staffansson
+ *
+ * This file is part of STC1000+.
+ *
+ * STC1000+ is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * STC1000+ is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with STC1000+.  If not, see <http://www.gnu.org/licenses/>.
+ *
+ */
+
+/* Initial EEPROM data, note temperatures are multiplied by 10 (for 1 decimal point precision) */
+#ifdef FAHRENHEIT
+	__code const int __at(0xF000) eedata[] = {
+			600, 24, 620, 24, 640, 24, 660, 24, 680, 144, 770, 48, 400, 0, 0, 0, 0, 0, 0, // Pr0 (SP0, dh0, ..., dh8, SP9)
+			600, 24, 620, 24, 640, 24, 660, 24, 680, 144, 770, 48, 400, 0, 0, 0, 0, 0, 0, // Pr0 (SP0, dh0, ..., dh8, SP9)
+			600, 24, 620, 24, 640, 24, 660, 24, 680, 144, 770, 48, 400, 0, 0, 0, 0, 0, 0, // Pr0 (SP0, dh0, ..., dh8, SP9)
+			600, 24, 620, 24, 640, 24, 660, 24, 680, 144, 770, 48, 400, 0, 0, 0, 0, 0, 0, // Pr0 (SP0, dh0, ..., dh8, SP9)
+			600, 24, 620, 24, 640, 24, 660, 24, 680, 144, 770, 48, 400, 0, 0, 0, 0, 0, 0, // Pr0 (SP0, dh0, ..., dh8, SP9)
+			600, 24, 620, 24, 640, 24, 660, 24, 680, 144, 770, 48, 400, 0, 0, 0, 0, 0, 0, // Pr0 (SP0, dh0, ..., dh8, SP9)
+			10, // Hysteresis (temperature * 10 that is 5 = 0.5)
+			0, // Temp correction (temperature * 10)
+			680, // Setpoint (temperature * 10)
+			0, // Current step (0-8)
+			0, // Current step duration (0-999 hours)
+			15, // Cooling delay (0-60 minutes)
+			2, // Heating delay,
+			6 // Run mode (0-5 running profile 0-5, 6 = thermostat mode)
+};
+#else // CELSIUS
+	__code const int __at(0xF000) eedata[] = {
+			160, 24, 170, 24, 180, 24, 190, 24, 200, 144, 250, 48, 40, 0, 0, 0, 0, 0, 0, // Pr0 (SP0, dh0, ..., dh8, SP9)
+			160, 24, 170, 24, 180, 24, 190, 24, 200, 144, 250, 48, 40, 0, 0, 0, 0, 0, 0, // Pr1 (SP0, dh0, ..., dh8, SP9)
+			160, 24, 170, 24, 180, 24, 190, 24, 200, 144, 250, 48, 40, 0, 0, 0, 0, 0, 0, // Pr2 (SP0, dh0, ..., dh8, SP9)
+			160, 24, 170, 24, 180, 24, 190, 24, 200, 144, 250, 48, 40, 0, 0, 0, 0, 0, 0, // Pr3 (SP0, dh0, ..., dh8, SP9)
+			160, 24, 170, 24, 180, 24, 190, 24, 200, 144, 250, 48, 40, 0, 0, 0, 0, 0, 0, // Pr4 (SP0, dh0, ..., dh8, SP9)
+			160, 24, 170, 24, 180, 24, 190, 24, 200, 144, 250, 48, 40, 0, 0, 0, 0, 0, 0, // Pr5 (SP0, dh0, ..., dh8, SP9)
+			5, // Hysteresis (temperature * 10 that is 5 = 0.5)
+			0, // Temp correction (temperature * 10)
+			200, // Setpoint (temperature * 10)
+			0, // Current step (0-8)
+			0, // Current step duration (0-999 hours)
+			15, // Cooling delay (0-60 minutes)
+			2, // Heating delay,
+			6 // Run mode (0-5 running profile 0-5, 6 = thermostat mode)
+	};
+#endif
