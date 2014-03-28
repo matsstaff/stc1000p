9b0e785cd9981363fd82d18aa71922ae7bd493c6
Mats Staffansson <mats.staffansson@gmail.com>
2014-03-28 07:44:42 +0100

    Default ramping to 'off'.

--- eepromdata.c
+++ eepromdata.c
@@ -36,7 +36,7 @@
 			0, // Current step duration (0-999 hours)
 			5, // Cooling delay (0-60 minutes)
 			2, // Heating delay (0-4 minutes)
-			1, // Ramping
+			0, // Ramping
 			6 // Run mode (0-5 running profile 0-5, 6 = thermostat mode)
 };
 #else // CELSIUS
@@ -54,7 +54,7 @@
 			0, // Current step duration (0-999 hours)
 			5, // Cooling delay (0-60 minutes)
 			2, // Heating delay (0-4 minutes)
-			1, // Ramping
+			0, // Ramping
 			6 // Run mode (0-5 running profile 0-5, 6 = thermostat mode)
 	};
 #endif
