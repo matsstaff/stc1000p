04c6cfc9f3bc9ba478ef2cdf780ece6b955ee16f
smcveigh <smcveigh@blackberry.com>
2016-01-22 05:12:45 -0500

    optimize range checking of adfilter value in ad_to_temp()

--- src/page0.c
+++ src/page0.c
@@ -871,11 +871,22 @@ static unsigned int read_ad(unsigned int adfilter){
 static int ad_to_temp(unsigned int adfilter){
 	unsigned char i;
 	long temp = 32;
-	unsigned char a = ((adfilter >> (FILTER_SHIFT-1)) & 0x3f); // Lower 6 bits
-	unsigned char b = ((adfilter >> (FILTER_SHIFT+5)) & 0x1f); // Upper 5 bits
-	unsigned char adfilter_l = adfilter >> 8;
-
-	if ((adfilter_l >= 248) || (adfilter_l <=8 )) {
+	unsigned char b = adfilter >> 8;
+	unsigned char a = (adfilter >> (FILTER_SHIFT-1)) & 0x3f;  // Lower 6 bits
+#if (FILTER_SHIFT >= 3)
+	b = b >> (FILTER_SHIFT+5-8);                              // Upper 5 bits
+	// range check that upper 8 bits are between [8..247].
+	if ((b >= (248 >> (FILTER_SHIFT+5-8))) || 
+#else
+#error This code was optimized for FILTER_SHIFT >= 3
+#endif
+#if (FILTER_SHIFT == 6)
+		// Minor optimization if FILTER_SHIFT = 6
+		(!b))
+#else
+		(b < (8 >> (FILTER_SHIFT+5-8)))
+#endif
+	{
 		state_flags.ad_badrange = 1;
 	}
 
